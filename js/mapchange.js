d31bad640d19edab4cc65621a348a3a4b94e7a61
scProjectsStory <138625070+scProjectsStory@users.noreply.github.com>
2023-09-28 09:09:45 +0800

    Update mapchange.js

--- js/mapchange.js
+++ js/mapchange.js
@@ -3,8 +3,10 @@
       //https://schuelerzahlen-zuerich.opendata.iwi.unibe.ch/App2/d3geo-2/ch_var/index.html#pop_female/1991
       //And the catogram function came from : https://github.com/emeeks/d3-cartogram
 
+      //The former effect of this page just like var fisheye = d3.fisheye.circular().distortion(4).focus([GlobalMapPathData[n].X,GlobalMapPathData[n].Y]).radius(matchData.Area*normalised_value*100)
+      //But that only consider the value, didn't consider the wards' area, so based on this situation, we can use cartogram function to make the effects.
       window.onload = function() {changeMapColor();refresh();};// Trigger the reset and colour change functions after the page be refreshed.
-      window.onhashchange = function() {DrawHcMap();}; // Call the url hash update function.
+      window.onhashchange = function() {updateAndDrawHcMap();}; // Call the url hash update function.
       var colour_map = {"1": "#5470c6", "2": "#91cd77", "3": "#f9c956"};
 
       function getIntroContentById(id) {
@@ -61,14 +63,14 @@
         if (selectedValue === "Category") {    // Update map colours according to selected indicators
             var listHTML = "<p>"+"<span style='background:"+colour_map["1"]+";'></span>"+"<span>West Borough</span>"+"</p>"+"<p>"+"<span style='background:"+colour_map["2"]+";'></span>"+"<span>Central Borough</span>"+"</p>"+"<p>"+"<span style='background:"+colour_map["3"]+";'></span>"+"<span>East Borough</span>"+"</p>";  //initialise the shade area color explain
             document.getElementById("shadecolor_explain").innerHTML = listHTML;
-            counties.transition().duration(700).attr("fill", function (e) {
+            wards_p.transition().duration(700).attr("fill", function (e) {
             return colour_map[e.properties.Category];});
         }else{
             var dataArray = Object.values(dataById); //Because I wanna to sort the data, first convert the data to an object type.
             x = dataArray.sort((a, b) => a[selectedValue] - b[selectedValue]);
             console.log(x)
             var ShadeColor = ["#487eb0", "#5352ed", "#c23616"];
-            counties.transition().duration(700).attr("fill", function (d, i) { //Sort the data into three groups and assign different colours to them.
+            wards_p.transition().duration(700).attr("fill", function (d, i) { //Sort the data into three groups and assign different colours to them.
                 var dataIndex = x.indexOf(dataById[d.properties.NAME]);
                 var colorIndex = Math.floor(dataIndex / 7); //A total of 21 wards in groups of 7
                 return ShadeColor[colorIndex];});
@@ -90,44 +92,39 @@
       var map = d3.select("#map-hc");
       var proj = d3.geo.albers() .origin([-0.158,51.6]).scale(570000).translate([400,250]); //albers projection, geographic oblique angle projection function，citation: https://vimsky.com/examples/usage/d3-js-geoalbers-function.html
       var path = d3.geo.path().projection(proj);
-      var layer = map.append("g").attr("id", "layer"), // Setting up the data binding layer
-          counties = layer.append("g").attr("id", "counties").selectAll("path");
-      var topology,geometries,rawData,dataById = {},
-          carto = d3.cartogram().projection(proj).properties(function(d) {return dataById[d.id];}).value(function(d) {return +d.properties[field];});
+      var layer = map.append("g").attr("id", "layer"), wards_p = layer.append("g").attr("id", "wards_p").selectAll("path"); // Setting up the data binding layer
+      var topology,geometries,rawData,dataById = {}, carto = d3.cartogram().projection(proj).properties(function(d) {return dataById[d.id];}).value(function(d) {return +d.properties[field];});
       var segmentized = location.search === "?segmentized",
           url = ["data","uk.topojson"].join("/");
       d3.json(url, function(topo) { //Reading HC area actual business data
         topology = topo;
-        geometries = topology.objects.counties.geometries;
+        geometries = topology.objects.wards_p.geometries;
         d3.csv("data/HCdata.csv", function(data) {
           rawData = data;
           dataById = d3.nest().key(function(d) { return d.NAME; }).rollup(function(d) { return d[0]; }).map(data);
-          init();
-        });
+          init();});
       });
 
       //init function is responsible for initialising the page elements.
       function init() {
-        var features = carto.features(topology, geometries), // map built
-            path = d3.geo.path().projection(proj);
-        counties = counties.data(features).enter().append("path").attr("class", "county") //give it path and color
+        var features = carto.features(topology, geometries),  path = d3.geo.path().projection(proj); // map built
+        wards_p = wards_p.data(features).enter().append("path").attr("class", "county") //give it path and color
             .attr("id", function(d) {return d.properties.NAME;})
             .attr("fill", function (e) {return colour_map[e.properties.Category]})
             .attr("d", path);
-        counties.append("title");
+        wards_p.append("title");
         DrawHcMap();
       }
 
       //reset element is responsible for the orientation of the page after it has been refreshed.
       function refresh() {
-        var features = carto.features(topology, geometries),
-            path = d3.geo.path().projection(proj);
-        counties.data(features).transition().duration(700).attr("fill", function (e) { //Colouring the map with the area category after refreshing it
+        var features = carto.features(topology, geometries), path = d3.geo.path().projection(proj);
+        wards_p.data(features).transition().duration(700).attr("fill", function (e) { //Colouring the map with the area category after refreshing it
                 return colour_map[e.properties.Category]
             })
             .attr("d", path);
         document.getElementById("introduction").innerHTML = "The map shows the information in Haringey 21 wards, please click top indexes or hover the mouse to the map to get the information"; //shade color introduction
-        counties.select("title").text(function(d) {return d.properties.NAME;});
+        wards_p.select("title").text(function(d) {return d.properties.NAME;});
         window.location.hash = "#none/";
         var selectBox = document.getElementById("shadeSelect");  // Get the value of the select color
         selectBox.value = "Category";
@@ -135,43 +132,29 @@
         document.getElementById("shadecolor_explain").innerHTML = listHTML;
       }
 
-      // The update function updates the map.
-      function update() {
-        body.classed("updating", true);
-        var key = field.key.replace("%d",""),
-            fmt = (typeof field.format === "function")? field.format: d3.format(field.format || ","),
-            value = function(d) {return +d.properties[key];},
-            values = counties.data().map(value).filter(function(n) {return !isNaN(n);}).sort(d3.ascending),
-            LCI = values[0],
-            HCI = values[values.length - 1];
-        var scale = d3.scale.linear().domain([LCI, HCI]).range([1, 100]); //scale of change
-        carto.value(function(d) {return scale(value(d));});
-        var features = carto(topology, geometries).features;
-        counties.data(features).select("title").text(function(d) {
-              return [d.properties.NAME, fmt(value(d))].join(": ");
-            });
-        counties.transition().duration(700).attr("d", carto.path);
+      function updateAndDrawHcMap() {
+          var parts = location.hash.substr(1).split("/"),
+              desiredFieldId = parts[0];
+          field = IndexesById[desiredFieldId] || Indexes[0];
+          Indexeselect.property("selectedIndex", Indexes.indexOf(field));
+          if (field.id === "none") {refresh();}
+          else {
+              var key = field.key.replace("%d", ""),
+                  fmt = (typeof field.format === "function") ? field.format : d3.format(field.format || ","),
+                  value = function (d) {return +d.properties[key]; },
+                  values = wards_p.data().map(value).filter(function (n) {return !isNaN(n);}).sort(d3.ascending),
+                  LCI = values[0],
+                  HCI = values[values.length - 1];
+              var scale = d3.scale.linear().domain([LCI, HCI]).range([1, 100]);
+              carto.value(function (d) {return scale(value(d));});
+              var features = carto(topology, geometries).features;
+              wards_p.data(features).select("title").text(function (d) {
+                  return [d.properties.NAME, fmt(value(d))].join(": ");
+              });
+              wards_p.transition().duration(700).attr("d", carto.path);
+          }
+          hashish.attr("href", function (href) { // Update hash href
+              return href + location.hash;
+          });
       }
 
-      // The function, set delay time and use update function
-      var DoUpdate = (function() {
-          var timeout;
-          return function() {
-          var args = arguments;
-          clearTimeout(timeout);
-          return timeout = setTimeout(function() {update.apply(null, arguments);}, 10);
-        };
-      })();
-
-      var hashish = d3.selectAll("a.hashish").datum(function() {return this.href;});
-
-      // parse hash function to refresh URLs for overall map updates
-      function DrawHcMap() {
-        var parts = location.hash.substr(1).split("/"),
-            desiredFieldId = parts[0];
-        field = IndexesById[desiredFieldId] || Indexes[0];
-        Indexeselect.property("selectedIndex", Indexes.indexOf(field));
-        if (field.id === "none") {refresh(); }
-        DoUpdate(); //do update
-        hashish.attr("href", function(href) {return href + location.hash;});
-      }
